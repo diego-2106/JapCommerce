e3905ccebee8c59f35d7c957804f3c0c23d9c746
diego-2106 <ez.mm.gg@gmail.com>
2023-08-07 10:30:45 -0300

    Creacion login y validacion

---
+++ js/login.js
@@ -0,0 +1,38 @@
+
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function (e) {
+    if (window.location.pathname !== "/index.html" && localStorage.username && localStorage.password) {
+        window.location.href = "index.html";
+    }
+
+    const usernameInput = document.getElementById("username");
+    const passwordInput = document.getElementById("password");
+    const msgError = document.getElementById("msg-error");
+
+    const login = () => {
+        localStorage.username = usernameInput.value;
+        localStorage.password = passwordInput.value;
+    }
+
+    document.addEventListener("submit", (event) => {
+        event.preventDefault();
+        if(/^[A-Za-z0-9]+(?:[_-][A-Za-z0-9]+)*$/.test(usernameInput.value)){
+            if(/^[a-zA-Z0-9]+$/.test(passwordInput.value)){
+                login();
+                window.location.replace("index.html");
+            }else{
+                msgError.innerHTML = "Contraseña no válida: Mínimo ocho caracteres, al menos una letra y un número";
+                msgError.style.display = "block";
+            }
+        }else{
+            msgError.innerHTML = "Nombre de usuario no válido: solo números, letras y guiones. No puede comenzar ni terminar con guiones";
+            msgError.style.display = "block";
+        }
+    });
+
+    document.getElementById('password').addEventListener('change',(event) => {
+        msgError.style.display = "none";
+    });
+});
