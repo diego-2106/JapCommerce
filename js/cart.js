9f0d11da158becb347809ad32381018b304711ff
gperez-dev <gonzaperez.uy@gmail.com>
2023-10-12 08:54:31 -0300

    Correcion parte1

--- js/cart.js
+++ js/cart.js
@@ -11,7 +11,26 @@ fetch(API)
 
     .then((data) => {
         let cartProd = data.articles;
-
+        let cartContainer = document.getElementById("cart-items");
         console.log(cartProd)
+
+        let subtotal = 0;
+        function CalcularSubtotal(cantidad, precio) {
+            return cantidad * precio;
+        }
+
+        cartProd.forEach((producto, index) => {
+            let cantidad = producto.count;
+            let precio = producto.unitCost;
+            let subtotalProducto = CalcularSubtotal(cantidad, precio);
+            subtotal += subtotalProducto;
+
+            cartContainer.innerHTML += `
+            <img src="${producto.image}" alt="imagenDeProducto" />
+            Producto: ${producto.name}, Precio Unidad: ${producto.currency} ${producto.unitCost} Cantidad: <input type="number" class="cantProd" value="${cantidad}" min="1" data-product-index=
+            Subtotal: <span id-"subtotalProducto${index}">${producto.currency} ${subtotalProducto}</span>
+            <br></br>
+            `;
+        });
     })
 
