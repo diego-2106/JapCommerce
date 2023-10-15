31955c92223dd4fe14fc9fb19c1d45412fdeea69
gperez-dev <gonzaperez.uy@gmail.com>
2023-10-15 17:34:49 -0300

    añadimos estilos al carrito

--- js/cart.js
+++ js/cart.js
@@ -26,27 +26,36 @@ fetch(API)
             subtotal += subtotalProducto;
 
             cartContainer.innerHTML += `
-            <img src="${producto.image}" alt="imagenDeProducto" />
-            Producto: ${producto.name}, Precio Unidad: ${producto.currency} ${producto.unitCost} Cantidad: <input type="number" class="cantProd" value="${cantidad}" min="1" data-product-index="${index}">
-            Subtotal: <span id="subtotalProducto${index}">${producto.currency} ${subtotalProducto}</span>
-            <br></br>
+            <div class="card d-flex flex-row align-items-center">
+            <img src="${producto.image}" alt="imagenDeProducto" width="200px" />
+            <div class="card-body">
+              <h5 class="card-title">Producto: ${producto.name}</h5>
+              <p class="card-text">
+                Precio Unidad: ${producto.currency} ${producto.unitCost}
+                <input type="number" class="input-cantidad form-control cantProd flex-grow" value="${cantidad}" min="1" data-product-index="${index}" />
+              </p>
+              Subtotal: <span id="subtotalProducto${index}">${producto.currency} ${subtotalProducto}</span>
+            </div>
+          </div>
+          
+          
             `;
 
-            
-        const cantidadInput = cartContainer.querySelector(`[data-product-index="${index}"]`);
-        cantidadInput.addEventListener('input', (event) => {
-        const cantidad = parseInt(event.target.value);
-        const Subtotal = CalcularSubtotal(cantidad, precio);
-        subtotal += Subtotal;
- 
-        
-        const subtotalSpan = cartContainer.querySelector(`#subtotalProducto${index}`);
-        subtotalSpan.textContent = `${producto.currency} ${Subtotal}`;
-    });
-});
 
-    //No funciona porque no hay un elemento todavia pero lo dejamos para mas adelante
-    const totalSpan = document.getElementById("cart-total");
-    totalSpan.textContent = `Total: ${data.currency} ${subtotal}`;
-});
+            const cantidadInput = cartContainer.querySelector(`[data-product-index="${index}"]`);
+            cantidadInput.addEventListener('input', (event) => {
+                const cantidad = parseInt(event.target.value);
+                const Subtotal = CalcularSubtotal(cantidad, precio);
+                subtotal += Subtotal;
+
+
+                const subtotalSpan = cartContainer.querySelector(`#subtotalProducto${index}`);
+                subtotalSpan.textContent = `${producto.currency} ${Subtotal}`;
+            });
+        });
+
+        //No funciona porque no hay un elemento todavia pero lo dejamos para mas adelante
+        const totalSpan = document.getElementById("cart-total");
+        totalSpan.textContent = `Total: ${data.currency} ${subtotal}`;
+    });
 
