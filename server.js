ce4ad045f9c912b67d39270cbbac7d04d06eb9be
emprincessniall <emiliarodriguez834@gmail.com>
2023-11-24 22:55:37 -0300

    Entrega 8 Punto 1

--- server.js
+++ server.js
@@ -4,13 +4,24 @@ const fs = require("fs");
 const port = 3000;
 
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(__dirname, '/public'));
 
 const path = require('path');
 
-app.get('/login', (req, res) => { 
+app.get('/', (req, res) => { 
     const filePath = path.join(__dirname, 'login.html');
     res.sendFile(filePath);
+   
+});
+app.post("/login", async (req, res) => {
+    const { emailInput, passwordInput } = req.body;
+  
+    if (emailInput === "" || passwordInput === "") {
+      return res.status(404).send("Los campos no pueden estar vacíos.");
+    }else{ res.redirect('/index.html'); }
+});
+app.get('/index.html', (req, res) => { 
+    res.sendFile(__dirname + '/index.html');
    
 });
 //1
@@ -29,13 +40,13 @@ app.get('/cats/cat.json', (req, res) => {
     res.json(JSON.parse(data));
 });
 
-app.get('/cats_products/...', (req, res) => {
-    const data = fs.readFileSync('./data/cats_products/...', 'utf8');
-    res.json(JSON.parse(data));
+app.get('/cats_products/:catID', (req, res) => {
+    const data = fs.readFileSync('./json/cats_products/:catID', 'utf8');
+    res.json(JSON.parse[req.params.catID]);
 });
 
-app.get('/products/...', (req, res) => {
-    const data = fs.readFileSync('./data/products/...', 'utf8');
+app.get('/products/:id', (req, res) => {
+    const data = fs.readFileSync('./json/products/:id', 'utf8');
     res.json(JSON.parse(data));
 });
 
