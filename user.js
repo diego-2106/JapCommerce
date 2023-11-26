856f3b2179b2cbc1d216a8021cd40aa503da09d2
ghostpanther777 <140995534+ghostpanther777@users.noreply.github.com>
2023-11-26 13:23:48 -0300

    ni yo sé que hice

--- user.js
+++ user.js
@@ -3,29 +3,56 @@ const app = express();
 const path = require('path');
 const fs = require("fs");
 const port = 3000;
+const mariadb = require('mariadb');
+const jwt = require('jsonwebtoken');
 
-app.use(express.static(path.join(__dirname, 'css')));
-app.use(express.static(path.join(__dirname, 'img')));
+// conexion
+const db = mariadb.createPool({
+
+    host: 'localhost',
+    user: 'root',
+    password: '123',
+    database: 'usuarios',
+    connectionLimit: 5 
+});
+
+app.use(express.json())
 
 app.get('/', (req, res) => { 
     const filePath = path.join(__dirname, './front/login.html');
     res.sendFile(filePath);
-   
+
 });
 app.post("/login", async (req, res) => {
-    const { emailInput, passwordInput } = req.body;
+    //res.redirect(__dirname, './front/index.html'); 
+    const { email, password } = req.body;
   
-    if (emailInput === "" || passwordInput === "") {
+    if (email === "" || password === "") {
       return res.status(404).send("Los campos no pueden estar vacíos.");
-    }else{ res.redirect('/index.html'); }
-});
-app.get('/index.html', (req, res) => { 
-    res.sendFile(__dirname + '/front/index.html');
-   
+    }
+  
+try {
+    const connection = await db.getConnection();
+    const result = await connection.query(
+        "SELECT * FROM usuario WHERE email = ? AND password = ?",
+        [email, password]
+    );
+
+    if (result.length === 0) {
+        const filePath = path.join(__dirname, './front/index.html');
+        res.sendFile(filePath);
+    }else {
+      res.status(401).send("Usuario o contraseña incorrectos");
+    }
+    
+    connection.release();
+  } catch (error) {
+    console.error("Error al procesar la solicitud:", error);
+    res.status(500).send("Error interno del servidor");
+  }
 });
 
-app.get('/index.html', (req, res) => { 
-    const filePath = path.join(__dirname, 'front', 'index.html');
-    res.sendFile(filePath);
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
 });
 
